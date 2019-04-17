09e296f3dcd5e846bfbd07244dfec7b4750df8de
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 14:35:48 -0500

    Added product factory refactor

---
+++ CodeSmells.Test/ProductFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace CodeSmells.Test
+{
+    public class ProductFactoryTests
+    {
+        [Fact]
+        public void can_create_product_instance_for_price_method_by_item()
+        {
+            var expected = new ProductByItem { ProductName = "Item", Quantity = 2, Price = 3m };
+            var actual = ProductFactory.Instance.CreateProductFor(PriceMethodEnum.PerItem, "Item", 3m, 2) as ProductByItem;
+            Assert.True(expected.Equals(actual));
+        }
+
+        [Fact]
+        public void can_create_product_instance_for_price_method_by_weight()
+        {
+            var expected = new ProductByWeight { ProductName = "Item", Weight = 2m, Price = 3m };
+            var actual = ProductFactory.Instance.CreateProductFor(PriceMethodEnum.PerPound, "Item", 3m, 2m) as ProductByWeight;
+            Assert.True(expected.Equals(actual));
+        }
+    }
+}
---
+++ CodeSmells/ProductFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CodeSmells
+{
+    public sealed class ProductFactory
+    {
+        private static Lazy<ProductFactory> instance = new Lazy<ProductFactory>(() => new ProductFactory());
+        public static ProductFactory Instance => instance.Value;
+        private Dictionary<PriceMethodEnum, Func<Product>> strategies;
+
+        private ProductFactory()
+        {
+            strategies = new Dictionary<PriceMethodEnum, Func<Product>>
+            {
+                {PriceMethodEnum.PerItem, () => new ProductByItem()},
+                {PriceMethodEnum.PerPound, () => new ProductByWeight()}
+            };
+        }
+
+        public Product CreateProductFor(PriceMethodEnum priceMethod, string name, decimal price, int quantity)
+        {
+            var product = strategies[priceMethod]();
+            product.ProductName = name;
+            product.Price = price;
+            product.Quantity = quantity;
+
+            return product;
+        }
+
+        public Product CreateProductFor(PriceMethodEnum priceMethod, string name, decimal price, decimal weight)
+        {
+            var product = strategies[priceMethod]();
+            product.ProductName = name;
+            product.Price = price;
+            product.Weight = weight;
+
+            return product;
+        }
+    }
+}
