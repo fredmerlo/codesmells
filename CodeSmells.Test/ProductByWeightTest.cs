16eb963a90afd7f47a5c3c0011cb6478312b0905
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 13:03:30 -0500

    Added product by weight refactor

---
+++ CodeSmells.Test/ProductByWeightTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace CodeSmells.Test
+{
+    public class ProductByWeightTest
+    {
+        [Fact]
+        public void product_by_weight_subclasses_product()
+        {
+            var expected = "CodeSmells.Product";
+            var actual = typeof(ProductByItem).BaseType.FullName;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void calculates_product_price_as_weight_times_price()
+        {
+            var productByWeight = new ProductByWeight { ProductName = "Item", Weight = 2, Price = 3m };
+            var expected = 2 * 3;
+            Assert.Equal(expected, productByWeight.ProductPrice);
+        }
+
+        [Fact]
+        public void to_string_returns_formatted_product_by_item_details()
+        {
+            var productByWeight = new ProductByWeight { ProductName = "Item", Weight = 2, Price = 3m };
+            var expected = "Item $6 (2 items at $3 per lb)";
+            Assert.Equal(expected, productByWeight.ToString());
+        }
+    }
+}
---
+++ CodeSmells/ProductByWeight.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeSmells
+{
+    public class ProductByWeight : Product
+    {
+        public override decimal ProductPrice => this.Weight.Value * this.Price;
+
+        public override string ToString()
+        {
+            return $"{this.ProductName} ${this.ProductPrice} ({this.Weight} items at ${this.Price} per lb)";
+        }
+    }
+}
