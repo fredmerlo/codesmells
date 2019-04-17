7d0b1979fe1981b3913bceafcd6e467848057e95
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 12:19:50 -0500

    Renamed test class, added product tostring test

---
+++ CodeSmells.Test/ProductTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace CodeSmells.Test
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void product_pricing_method_is_enum_type_price_method()
+        {
+            var expected = "CodeSmells.PriceMethodEnum";
+            var actual = typeof(Product).GetField("PricingMethod").FieldType.FullName;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void product_is_abstract_class()
+        {
+            var actual = typeof(Product).IsAbstract;
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void product_price_property_is_abstract()
+        {
+            var actual = typeof(Product).GetProperty("ProductPrice").GetMethod.IsAbstract;
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void product_to_string_is_abstract()
+        {
+            var actual = typeof(Product).GetMethod("ToString").IsAbstract;
+            Assert.True(actual);
+        }
+    }
+}
