c50034c77d705c0de8b1c3624427a81079c0c226
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 10:21:10 -0500

    Added refactor test for price method

---
+++ CodeSmells.Test/CodeSmellTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace CodeSmells.Test
+{
+    public class CodeSmellTests
+    {
+        [Fact]
+        public void product_pricing_method_is_enum_type_price_method()
+        {
+            var actual = new Product();
+            Assert.IsType<PriceMethodEnum>(actual.PricingMethod);
+        }
+    }
+}
