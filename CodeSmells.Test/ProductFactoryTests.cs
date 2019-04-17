f8cdd962f962c08d2a7cfe7f982b0373c0db25da
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 15:48:44 -0500

    Fix for failing test due to missing pricing method assignment

--- CodeSmells.Test/ProductFactoryTests.cs
+++ CodeSmells.Test/ProductFactoryTests.cs
@@ -7,7 +7,7 @@ namespace CodeSmells.Test
         [Fact]
         public void can_create_product_instance_for_price_method_by_item()
         {
-            var expected = new ProductByItem { ProductName = "Item", Quantity = 2, Price = 3m };
+            var expected = new ProductByItem { ProductName = "Item", Quantity = 2, Price = 3m, PricingMethod = PriceMethodEnum.PerItem};
             var actual = ProductFactory.Instance.CreateProductFor(PriceMethodEnum.PerItem, "Item", 3m, 2) as ProductByItem;
             Assert.True(expected.Equals(actual));
         }
@@ -15,7 +15,7 @@ namespace CodeSmells.Test
         [Fact]
         public void can_create_product_instance_for_price_method_by_weight()
         {
-            var expected = new ProductByWeight { ProductName = "Item", Weight = 2m, Price = 3m };
+            var expected = new ProductByWeight { ProductName = "Item", Weight = 2m, Price = 3m, PricingMethod = PriceMethodEnum.PerPound};
             var actual = ProductFactory.Instance.CreateProductFor(PriceMethodEnum.PerPound, "Item", 3m, 2m) as ProductByWeight;
             Assert.True(expected.Equals(actual));
         }
