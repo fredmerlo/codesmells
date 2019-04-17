eace60f66c5e8ad70302c5bc091d349c189d7cc5
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 10:54:04 -0500

    Refactored price method enum test to use type full name

--- CodeSmells.Test/CodeSmellTests.cs
+++ CodeSmells.Test/CodeSmellTests.cs
@@ -7,8 +7,9 @@ namespace CodeSmells.Test
         [Fact]
         public void product_pricing_method_is_enum_type_price_method()
         {
-            var actual = new Product();
-            Assert.IsType<PriceMethodEnum>(actual.PricingMethod);
+            var expected = "CodeSmells.PriceMethodEnum";
+            var actual = typeof(Product).GetField("PricingMethod").FieldType.FullName;
+            Assert.Equal(expected, actual);
         }
     }
 }
