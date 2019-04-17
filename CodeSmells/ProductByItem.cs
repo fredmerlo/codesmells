42538feceded0feeedb1b4949f05aabfaff7fa35
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 14:35:07 -0500

    Implemented IEquality for testabilty

--- CodeSmells/ProductByItem.cs
+++ CodeSmells/ProductByItem.cs
@@ -2,13 +2,23 @@
 
 namespace CodeSmells
 {
-    public class ProductByItem : Product
+    public class ProductByItem : Product, IEquatable<Product>
     {
         public override decimal ProductPrice => this.Quantity.Value * this.Price;
 
         public override string ToString()
         {
             return $"{this.ProductName} ${this.ProductPrice} ({this.Quantity} items at ${this.Price} each)";
+        }
+
+        public bool Equals(Product other)
+        {
+            if (other == null)
+                return false;
+
+            return this.ProductName.Equals(other.ProductName) &&
+                   this.Quantity.Value.Equals(other.Quantity.Value) &&
+                   this.Price.Equals(other.Price);
         }
     }
 }
--- CodeSmells/ProductByWeight.cs
+++ CodeSmells/ProductByWeight.cs
@@ -1,12 +1,24 @@
-﻿namespace CodeSmells
+﻿using System;
+
+namespace CodeSmells
 {
-    public class ProductByWeight : Product
+    public class ProductByWeight : Product, IEquatable<Product>
     {
         public override decimal ProductPrice => this.Weight.Value * this.Price;
 
         public override string ToString()
         {
             return $"{this.ProductName} ${this.ProductPrice} ({this.Weight} items at ${this.Price} per lb)";
+        }
+
+        public bool Equals(Product other)
+        {
+            if (other == null)
+                return false;
+
+            return this.ProductName.Equals(other.ProductName) &&
+                   this.Weight.Value.Equals(other.Weight.Value) &&
+                   this.Price.Equals(other.Price);
         }
     }
 }
