6bdfd275fcfbfc520351ae8c8f261848fd2ada0c
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 12:55:24 -0500

    Added product by item refactor

---
+++ CodeSmells.Test/ProductByItemTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace CodeSmells.Test
+{
+    public class ProductByItemTest
+    {
+        [Fact]
+        public void product_by_item_subclasses_product()
+        {
+            var expected = "CodeSmells.Product";
+            var actual = typeof(ProductByItem).BaseType.FullName;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void calculates_product_price_as_quantity_times_price()
+        {
+            var productByItem = new ProductByItem { ProductName = "Item", Quantity = 2, Price = 3m};
+            var expected = 2 * 3;
+            Assert.Equal(expected, productByItem.ProductPrice);
+        }
+
+        [Fact]
+        public void to_string_returns_formatted_product_by_item_details()
+        {
+            var productByItem = new ProductByItem { ProductName = "Item", Quantity = 2, Price = 3m };
+            var expected = "Item $6 (2 items at $3 each)";
+            Assert.Equal(expected, productByItem.ToString());
+        }
+    }
+}
---
+++ CodeSmells/ProductByItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeSmells
+{
+    public class ProductByItem : Product
+    {
+        public override decimal ProductPrice => this.Quantity.Value * this.Price;
+
+        public override string ToString()
+        {
+            return $"{this.ProductName} ${this.ProductPrice} ({this.Quantity} items at ${this.Price} each)";
+        }
+    }
+}
