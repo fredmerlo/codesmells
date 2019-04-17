3f21c9f7c99b4129b856b2c74eba345d427cbfd1
Fred Merlo <fredmerlo@gmail.com>
2019-04-17 15:04:25 -0500

    Refactor main

--- CodeSmells/Program.cs
+++ CodeSmells/Program.cs
@@ -9,21 +9,11 @@ namespace CodeSmells
         {
             var order = new Order
             {
-                Name = "John Doe",
-                Products = new List<Product> {
-                    new Product {
-                        ProductName = "Pulled Pork",
-                        Price = 6.99m,
-                        Weight = 0.5m,
-                        PricingMethod = PriceMethodEnum.PerPound
-                    },
-
-                    new Product {
-                        ProductName = "Coke",
-                        Price = 3m,
-                        Quantity = 2,
-                        PricingMethod = PriceMethodEnum.PerItem
-                    }
+                Name = "Foo",
+                Products = new List<Product>
+                {
+                    ProductFactory.Instance.CreateProductFor(PriceMethodEnum.PerItem, "Item1", 3m, 2),
+                    ProductFactory.Instance.CreateProductFor(PriceMethodEnum.PerPound, "Item2", 3m, 2m)
                 }
             };
 
